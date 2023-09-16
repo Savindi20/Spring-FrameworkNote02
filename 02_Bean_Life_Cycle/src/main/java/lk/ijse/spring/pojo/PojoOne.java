3ef29ef8fb08ea40e2cdd48951511822e09f8bce
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-09-16 10:08:21 +0530

    ApplicationContextAware added

--- 02_Bean_Life_Cycle/src/main/java/lk/ijse/spring/pojo/PojoOne.java
+++ 02_Bean_Life_Cycle/src/main/java/lk/ijse/spring/pojo/PojoOne.java
@@ -4,10 +4,12 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.BeanFactoryAware;
 import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 @Component
-public class PojoOne implements BeanNameAware, BeanFactoryAware {
+public class PojoOne implements BeanNameAware, BeanFactoryAware, ApplicationContextAware {
     public PojoOne(){
         System.out.println("PojoOne: Instantiated");
     }
@@ -20,5 +22,10 @@ public class PojoOne implements BeanNameAware, BeanFactoryAware {
     @Override
     public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
         System.out.println("PojoOne: BeanFactory Aware");
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("PojoOne: Application Context Aware");
     }
 }
