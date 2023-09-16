226fcef6bc75c705ebfa44d16e3b4d91362e1773
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-09-16 10:22:06 +0530

    disposable bean added

--- 02_Bean_Life_Cycle/src/main/java/lk/ijse/spring/pojo/PojoOne.java
+++ 02_Bean_Life_Cycle/src/main/java/lk/ijse/spring/pojo/PojoOne.java
@@ -1,16 +1,13 @@
 package lk.ijse.spring.pojo;
 
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.BeanFactoryAware;
-import org.springframework.beans.factory.BeanNameAware;
-import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.*;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 @Component
-public class PojoOne implements BeanNameAware, BeanFactoryAware, ApplicationContextAware, InitializingBean {
+public class PojoOne implements BeanNameAware, BeanFactoryAware, ApplicationContextAware, InitializingBean, DisposableBean {
     public PojoOne(){
         System.out.println("PojoOne: Instantiated");
     }
@@ -33,5 +30,10 @@ public class PojoOne implements BeanNameAware, BeanFactoryAware, ApplicationCont
     @Override
     public void afterPropertiesSet() throws Exception {
         System.out.println("PojoOne: Initializing Bean: Bean is Ready for Use");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("PojoOne: Destroyed");
     }
 }
