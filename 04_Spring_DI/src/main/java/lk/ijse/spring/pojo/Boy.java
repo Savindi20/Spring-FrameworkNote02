2a73bebf91018fa73cf3fc83225335f9e523fdd2
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-09-16 19:53:15 +0530

    spring di module added

---
+++ 04_Spring_DI/src/main/java/lk/ijse/spring/pojo/Boy.java
@@ -0,0 +1,10 @@
+package lk.ijse.spring.pojo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Boy {
+    public Boy(){
+        System.out.println("Boy: Instantiated");
+    }
+}
