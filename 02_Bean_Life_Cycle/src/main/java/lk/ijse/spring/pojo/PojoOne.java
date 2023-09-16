c7f14a78d5651e1adb18d403fce3f87eb59afc2f
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-09-16 10:01:13 +0530

    bean name, factory aware added

--- 02_Bean_Life_Cycle/src/main/java/lk/ijse/spring/pojo/PojoOne.java
+++ 02_Bean_Life_Cycle/src/main/java/lk/ijse/spring/pojo/PojoOne.java
@@ -1,10 +1,24 @@
 package lk.ijse.spring.pojo;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.stereotype.Component;
 
 @Component
-public class PojoOne {
+public class PojoOne implements BeanNameAware, BeanFactoryAware {
     public PojoOne(){
         System.out.println("PojoOne: Instantiated");
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        System.out.println("PojoOne: Bean Name Aware: "+name);
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("PojoOne: BeanFactory Aware");
     }
 }
