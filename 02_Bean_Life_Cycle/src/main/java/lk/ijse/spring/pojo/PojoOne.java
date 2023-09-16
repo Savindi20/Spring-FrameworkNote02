5948b38fe36ccc6c7c349f9cac5f8f6fe5550f69
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-09-16 10:12:18 +0530

    InitializingBean

--- 02_Bean_Life_Cycle/src/main/java/lk/ijse/spring/pojo/PojoOne.java
+++ 02_Bean_Life_Cycle/src/main/java/lk/ijse/spring/pojo/PojoOne.java
@@ -4,12 +4,13 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.BeanFactoryAware;
 import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 @Component
-public class PojoOne implements BeanNameAware, BeanFactoryAware, ApplicationContextAware {
+public class PojoOne implements BeanNameAware, BeanFactoryAware, ApplicationContextAware, InitializingBean {
     public PojoOne(){
         System.out.println("PojoOne: Instantiated");
     }
@@ -27,5 +28,10 @@ public class PojoOne implements BeanNameAware, BeanFactoryAware, ApplicationCont
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
         System.out.println("PojoOne: Application Context Aware");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("PojoOne: Initializing Bean: Bean is Ready for Use");
     }
 }
