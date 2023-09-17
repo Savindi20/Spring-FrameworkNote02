b1678e2102bc4b84ed29226a83337dfb77006eb9
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-09-17 15:08:17 +0530

    @ImportResource introduced

--- 06_Spring_Configuration/src/main/java/lk/ijse/spring/config/AppConfig.java
+++ 06_Spring_Configuration/src/main/java/lk/ijse/spring/config/AppConfig.java
@@ -2,9 +2,14 @@ package lk.ijse.spring.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
 
 @Configuration
-@ComponentScan(basePackages = "lk.ijse.spring")
+@ComponentScan(basePackages = "lk.ijse.spring.pojo")
+@Import({ConfigOne.class, ConfigTwo.class})
+//@ImportResource("classpath:abcd.xml")
+//@ImportResource("filepath:abcd.xml")
 public class AppConfig {
     public AppConfig(){
         System.out.println("AppConfig: Instantiated");
