6309db05f58bf973a8281d26bd587b2250de2938
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-09-22 11:10:42 +0530

    @Autowired with reduired false

--- 07_Runtime_Value_Injection/src/main/java/lk/ijse/spring/pojo/Customer.java
+++ 07_Runtime_Value_Injection/src/main/java/lk/ijse/spring/pojo/Customer.java
@@ -1,5 +1,6 @@
 package lk.ijse.spring.pojo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -23,10 +24,16 @@ public class Customer {
 //        System.out.println("Customer: Instantiated "+age);
 //    }
 
-    public Customer(@Value("Amal, Kamal, Nimal") String names[]){
+    @Autowired(required = false)// mehem thibboth spring run krnne parameter count eka vadima constructor ekaii
+    public Customer(@Value("Amal, Kamal, Nimal") String names[], @Value("10") int i, @Value("20") int c){
         System.out.println("Customer: Instantiated ");
         for (String name : names){
             System.out.println(name);
         }
+    }
+
+    @Autowired(required = false)
+    public Customer(@Value("Amal") String name, @Value("10") int i){
+        System.out.println("Customer: Instantiated "+name);
     }
 }
