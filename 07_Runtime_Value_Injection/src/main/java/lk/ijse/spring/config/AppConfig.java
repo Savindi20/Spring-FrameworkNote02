ec5932d7e7916476a294ef3ae734229d09c93976
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-09-22 10:46:55 +0530

    Value annotation added

--- 07_Runtime_Value_Injection/src/main/java/lk/ijse/spring/config/AppConfig.java
+++ 07_Runtime_Value_Injection/src/main/java/lk/ijse/spring/config/AppConfig.java
@@ -1,5 +1,6 @@
 package lk.ijse.spring.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -7,7 +8,9 @@ import org.springframework.context.annotation.Import;
 @Configuration
 @ComponentScan(basePackages = "lk.ijse.spring.pojo")
 public class AppConfig {
-    public AppConfig(){
-        System.out.println("AppConfig: Instantiated");
+
+    @Bean
+    public String setD(){
+        return "Hello Hello";
     }
 }
--- 07_Runtime_Value_Injection/src/main/java/lk/ijse/spring/pojo/Customer.java
+++ 07_Runtime_Value_Injection/src/main/java/lk/ijse/spring/pojo/Customer.java
@@ -1,10 +1,15 @@
 package lk.ijse.spring.pojo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Customer {
-    public Customer(){
-        System.out.println("Customer: Instantiated");
+//    public Customer(){
+//        System.out.println("DefaultCustomer: Instantiated ");
+//    }
+
+    public Customer(@Value("IJSE") String name){
+        System.out.println("Customer: Instantiated "+name);
     }
 }
