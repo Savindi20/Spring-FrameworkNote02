f2d170c98eb77dd7b061f7192b2210f68045cc8e
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-09-28 17:26:21 +0530

    customer controller updated

--- 09_Spring_Web_MVC/src/main/java/lk/ijse/spring/config/WebAppConfig.java
+++ 09_Spring_Web_MVC/src/main/java/lk/ijse/spring/config/WebAppConfig.java
@@ -1,12 +1,15 @@
 package lk.ijse.spring.config;
 
+import lk.ijse.spring.controller.CustomerController;
+import lk.ijse.spring.pojo.SpringBeanOne;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = "lk.ijse.spring")
+//@ComponentScan(basePackages = {"lk.ijse.spring.pojo","lk.ijse.spring.controller"})
+@ComponentScan(basePackageClasses = {CustomerController.class, SpringBeanOne.class})
 public class WebAppConfig {
     public WebAppConfig() {
         System.out.println("WebAppConfig: Instantiate");
