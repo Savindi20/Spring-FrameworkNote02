4dfbac8531de8f70fdaaadfba66bd5c90270d8c6
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-09-28 23:41:45 +0530

    add new module for restController

--- 10_Controller/src/main/java/lk/ijse/spring/config/WebAppConfig.java
+++ 10_Controller/src/main/java/lk/ijse/spring/config/WebAppConfig.java
@@ -1,6 +1,7 @@
 package lk.ijse.spring.config;
 
 import lk.ijse.spring.controller.CustomerController;
+import lk.ijse.spring.controller.ItemController;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -10,7 +11,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackageClasses = {CustomerController.class})
+@ComponentScan(basePackageClasses = {CustomerController.class, ItemController.class})
 public class WebAppConfig {
 
     @Bean
---
+++ 11_ResetController/src/main/java/lk/ijse/spring/controller/CustomerController.java
@@ -0,0 +1,20 @@
+package lk.ijse.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+@RequestMapping("/customer")
+public class CustomerController {
+    public CustomerController() {
+        System.out.println("Customer Controller");
+    }
+
+    @GetMapping
+    public String testOne(){
+        return "Hello Customer Mapping";
+    }
+}
