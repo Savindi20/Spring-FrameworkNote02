1c7b653526d261568e126263a6fcdb8b72bf3685
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-09-30 21:56:02 +0530

    12_Mapping specs updated with full specs

--- 12_Mapping_Spec/src/main/java/lk/ijse/spring/config/WebAppConfig.java
+++ 12_Mapping_Spec/src/main/java/lk/ijse/spring/config/WebAppConfig.java
@@ -1,13 +1,11 @@
 package lk.ijse.spring.config;
 
-import lk.ijse.spring.controller.OneController;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@Configuration
 @EnableWebMvc
-@ComponentScan(basePackageClasses = {OneController.class})
+@Configuration
+@ComponentScan(basePackages = "lk.ijse.spring.controller")
 public class WebAppConfig {
-
 }
--- 12_Mapping_Spec/src/main/java/lk/ijse/spring/config/WebRootConfig.java
+++ 12_Mapping_Spec/src/main/java/lk/ijse/spring/config/WebRootConfig.java
@@ -4,7 +4,4 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class WebRootConfig {
-    public WebRootConfig() {
-        System.out.println("WebRootConfig: Instantiate");
-    }
 }
