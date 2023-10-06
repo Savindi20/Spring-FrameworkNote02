f2cbbb1b9768299717609c57c9e57ff24a5d2882
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-10-06 14:51:11 +0530

    content update with entity manager factory

--- 14_Spring_Data_JPA/src/main/java/lk/ijse/spring/config/JPAConfig.java
+++ 14_Spring_Data_JPA/src/main/java/lk/ijse/spring/config/JPAConfig.java
@@ -1,7 +1,20 @@
 package lk.ijse.spring.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class JPAConfig {
+
+    //database access (username, password, name, url)
+    //spring data JPA need a vendor to run ORM
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource ds, JpaVendorAdapter vad){
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        return factory;
+    }
 }
