c39237fba0f1f7bd427e9d836e60328de72cfc65
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-10-06 18:25:33 +0530

    customer controller updated

--- 14_Spring_Data_JPA/src/main/java/lk/ijse/spring/controller/CustomerController.java
+++ 14_Spring_Data_JPA/src/main/java/lk/ijse/spring/controller/CustomerController.java
@@ -1,8 +1,20 @@
 package lk.ijse.spring.controller;
 
+import lk.ijse.spring.dao.CustomerDAO;
+import lk.ijse.spring.dto.CustomerDTO;
+import lk.ijse.spring.entity.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/customer")
 public class CustomerController {
+
+    @Autowired
+    CustomerDAO customerDAO;
+
+    @PostMapping
+    public void addCustomer(Customer dto){
+        customerDAO.save(dto);
+    }
 }
