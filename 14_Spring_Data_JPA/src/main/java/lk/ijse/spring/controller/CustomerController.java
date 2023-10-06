94a44f658d4c98cd0c6a33a5780cbd5ad1963f47
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-10-06 19:23:08 +0530

    update with crud operations

--- 14_Spring_Data_JPA/src/main/java/lk/ijse/spring/controller/CustomerController.java
+++ 14_Spring_Data_JPA/src/main/java/lk/ijse/spring/controller/CustomerController.java
@@ -6,6 +6,8 @@ import lk.ijse.spring.entity.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/customer")
 public class CustomerController {
@@ -16,5 +18,25 @@ public class CustomerController {
     @PostMapping
     public void addCustomer(Customer dto){
         customerDAO.save(dto);
+    }
+
+    @DeleteMapping(params = {"id"})
+    public void deleteCustomer(String id){
+        customerDAO.deleteById("C001");
+    }
+
+    @GetMapping
+    public List<Customer> getAllCustomer(){
+         return customerDAO.findAll();
+    }
+
+    @GetMapping(params = {"id"})
+    public Customer findCustomer(String id){
+        return customerDAO.findById(id).get();
+    }
+
+    @PutMapping
+    public void updateCustomer(@RequestBody Customer c){
+        customerDAO.save(c);
     }
 }
