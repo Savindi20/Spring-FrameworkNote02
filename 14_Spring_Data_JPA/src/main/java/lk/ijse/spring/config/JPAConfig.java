211d9baaec42b29fa291b750b06a42203c2c0afb
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-10-06 15:10:54 +0530

    databse access Datasaource added

--- 14_Spring_Data_JPA/src/main/java/lk/ijse/spring/config/JPAConfig.java
+++ 14_Spring_Data_JPA/src/main/java/lk/ijse/spring/config/JPAConfig.java
@@ -2,6 +2,7 @@ package lk.ijse.spring.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 
@@ -14,10 +15,20 @@ public class JPAConfig {
     //spring data JPA need a vendor to run ORM
     @Bean
     public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource ds, JpaVendorAdapter vad){
-        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        LocalContainerEntityManagerFactoryBean factory= new LocalContainerEntityManagerFactoryBean();
         factory.setDataSource(ds);
         factory.setJpaVendorAdapter(vad);
         factory.setPackagesToScan("lk.ijse.spring.entity");
         return factory;
+    }
+
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource ds= new DriverManagerDataSource();
+        ds.setUsername("root");
+        ds.setPassword("1234");
+        ds.setDriverClassName("com.mysql.jdbc.Driver");
+        ds.setUrl("jdbc:mysql://localhost:3306/d2?createDatabaseIfNotExist=true");
+        return ds;
     }
 }
