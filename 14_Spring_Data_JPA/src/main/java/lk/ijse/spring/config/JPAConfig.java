a18fa4daf51506d30a1d8fec33ff937a9d0952e9
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-10-06 15:32:03 +0530

    transactionManager added

--- 14_Spring_Data_JPA/src/main/java/lk/ijse/spring/config/JPAConfig.java
+++ 14_Spring_Data_JPA/src/main/java/lk/ijse/spring/config/JPAConfig.java
@@ -3,11 +3,14 @@ package lk.ijse.spring.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 @Configuration
@@ -43,4 +46,10 @@ public class JPAConfig {
         va.setShowSql(true);
         return va;
     }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory factory){
+        return new JpaTransactionManager(factory);
+    }
+
 }
